32b3e769f9e78337bdedd4d22bd32fdf49be10f0
LiuAnBoy <eric141886@gmail.com>
2024-12-31 03:34:42 +0800

    feat(app): remove not use part

--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -1,15 +1,13 @@
 import cors from "cors";
 import { Application } from "express";
 
-import ConfigService from "../providers/config";
 import { ConsoleHandler } from "../utils/consoleHandler";
 
 class CORS {
   public static mount(_express: Application): Application {
-    const config = ConfigService.getInstance().getConfig();
     const logger = ConsoleHandler.getInstance("Middleware");
     const options = {
-      origin: config.APP_URL,
+      origin: "*",
       optionsSuccessStatus: 200, // Some legacy browsers choke on 204
     };
 
